b48973cea776deafc4a5013a4b44f90fac4ce622
aarush248 <71240722+aarush248@users.noreply.github.com>
2021-05-14 15:33:32 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+fill("white")
+textSize(15)
+text("N U F N O T C I", 700,150)
+fill("lightblue")
+text("Hint: Like a recipe", 700,170)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+textSize(15)
+text("A T A D A S B E", 100,350)
+fill("lightblue")
+text("Hint: What will you use to store data?", 100,370)
+// add code for displaying rest of the names and the hints.
+}
